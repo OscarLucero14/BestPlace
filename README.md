f94ae2b95cae3db84537a6ea7e4ddff07545584e
Oscar L <oscarlucero14@hotmail.com>
2016-04-06 12:14:40 -0400

    update app with jQuery Mobile

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # BestPlace
-Assignment1 PROG8110
+This application is focus on promote touristic places in Colombia. For this purpose, people can easily navigate into the app, and it will have provide to the user the following information:
+•	Shows a map with the 5 main points of interest in Colombia
+•	Describes the 7 most important sites and monuments
+•	Includes pictures of the main places before traveling
+•	Includes images and names of typical food of these main places
+This app will be the best way to allow tourists enjoy a complete experience before traveling to Colombia.
--- js/form.js
+++ js/form.js
@@ -1,12 +1,7 @@
 var app = angular.module("myapp", [
     'mobile-angular-ui',
 
-    // touch/drag feature: this is from 'mobile-angular-ui.gestures.js'
-    // it is at a very beginning stage, so please be careful if you like to use
-    // in production. This is intended to provide a flexible, integrated and and
-    // easy to use alternative to other 3rd party libs like hammer.js, with the
-    // final pourpose to integrate gestures into default ui interactions like
-    // opening sidebars, turning switches on/off ..
+
     'mobile-angular-ui.gestures',
     'uiGmapgoogle-maps'
 ])
